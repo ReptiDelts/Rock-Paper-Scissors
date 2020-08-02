79158f3e424dbab7d104585da40d0e3f6c86ca75
ReptiDelts <69094346+ReptiDelts@users.noreply.github.com>
2020-08-01 23:47:17 -0400

    Add files via upload

--- rock paper scissors.py
+++ rock paper scissors.py
@@ -4,6 +4,16 @@ choices = ["Rock", "Paper", "Scissors"]
 Wins = 0
 Losses = 0 
 
+def counter_func():
+    if 1 == Wins and 1 == Losses: 
+        print("You have", Win, "Win and", Losses, "Loss.")
+    elif 1 == Losses:
+        print("You have", Wins, "Wins and", Losses, "Loss.")
+    elif 1 == Wins:
+        print("You have", Wins, "Win and", Losses, "Losses.")
+    else:
+        print("You have", Wins, "Wins and", Losses, "Losses.")
+
 player = False
 
 while player == False:
@@ -14,60 +24,41 @@ while player == False:
     print("The computer chose",computer, "and you chose", player)
     if player == computer:
         print("Tie!")
-        if Wins == 1 or Losses == 1:
-            print("You have", Wins, "Win and", Losses, "Loss.")
-        else:
-            print("You have", Wins, "Wins and", Losses, "Losses.")
+        counter_func()
     elif player == "Rock":
         if computer == "Paper":
             print("You lose!", computer, "wraps", player) 
             Losses = Losses + 1
-            if Wins == 1 or Losses == 1:
-                print("You have", Wins, "Win and", Losses, "Loss.")
-            else:
-                print("You have", Wins, "Wins and", Losses, "Losses.")  
+            counter_func()  
         else:
             print("You win!", player, "crushes", computer)
-            Wins= Wins + 1
-            if Wins == 1 or Losses == 1:
-                print("You have", Wins, "Win and", Losses, "Loss.")
-            else:
-                print("You have", Wins, "Wins and", Losses, "Losses.") 
+            Wins = Wins + 1
+            counter_func() 
     elif player == "Paper":
         if computer == "Scissors":
             print("You lose!", computer, "cuts", player)
-            Losses= Losses + 1
-            if Wins == 1 or Losses == 1:
-                print("You have", Wins, "Win and", Losses, "Loss.")
-            else:
-                print("You have", Wins, "Wins and", Losses, "Losses.") 
+            Losses = Losses + 1
+            counter_func()  
         else: 
             print("You win!", player, "wraps", computer)
-            Wins= Wins + 1
-            if Wins == 1 or Losses == 1:
-                print("You have", Wins, "Win and", Losses, "Loss.")
-            else:
-                print("You have", Wins, "Wins and", Losses, "Losses.")  
+            Wins = Wins + 1
+            counter_func()   
     elif player == "Scissors":
         if computer == "Rock":
             print("You lose!", computer, "crushes", player)
-            Losses= Losses + 1
-            if Wins == 1 or Losses == 1:
-                print("You have", Wins, "Win and", Losses, "Loss.")
-            else:
-                print("You have", Wins, "Wins and", Losses, "Losses.") 
+            Losses = Losses + 1
+            counter_func()  
         else:
             print("You win!", player, "cuts", computer)
-            Wins= Wins + 1
-            if Wins == 1 or Losses == 1:
-                print("You have", Wins, "Win and", Losses, "Loss.")
-            else:
-                print("You have", Wins, "Wins and", Losses, "Losses.")   
+            Wins = Wins + 1
+            print(counter_func())   
     else:
         print("That's not a valid move. Check your spelling.")
-        break
-
     player = False
+    computer = choices[r]
+      
+    
+        
 
 
         
